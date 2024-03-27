929d0a489c7f5268ecfd00317497b1b7b5ea67c3
AlexanderFLFA <164126091+AlexanderFLFA@users.noreply.github.com>
2024-03-27 14:23:59 -0600

    Delete Mensajeria directory

--- Mensajeria/script.js
+++
@@ -1,7 +0,0 @@
-function sendMessage() {
-    var message = document.getElementById('message-input').value;
-    if (message.trim() !== '') {
-        document.getElementById('chat-box').innerHTML += '<p>' + message + '</p>';
-        document.getElementById('message-input').value = '';
-    }
-}
